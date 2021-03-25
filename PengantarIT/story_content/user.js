307613f143deb9b66ee37da58252ceb176ed1601
aldip4886 <79340066+aldip4886@users.noreply.github.com>
2021-03-25 17:15:26 +0700

    update

---
+++ PengantarIT/story_content/user.js
@@ -0,0 +1,117 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6SzPC8GXipI":
+        Script1();
+        break;
+      case "6ME13b4qUI8":
+        Script2();
+        break;
+      case "6l4G15XYqQa":
+        Script3();
+        break;
+      case "60ps1HpkYqL":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  //load the scripts dynamically into the head of the document
+function add_line() {
+    var line = document.createElement("audio");
+    var head=document.getElementsByTagName('body')[0];
+    line.type = "audio/mp3";
+    line.src="";
+    line.id="bgSong" ;
+	line.autoplay = true;
+	line.loop = true;
+    head.appendChild(line);
+}
+
+//but we only want to add these once!
+if(document.getElementById('bgSong')==null){
+	add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.4;
+}
+
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+
+var audio = document.getElementById('bgSong');
+audio.src=Location+"[No Copyright Music] Uplifting & Inspiring Corporate Background - Ramol (Original).mp3";
+audio.load();
+audio.play();
+}
+
+function Script3()
+{
+  var player=GetPlayer();
+var theScore=player.GetVar("nilai");
+top.postMessage(theScore,"*");
+
+}
+
+function Script4()
+{
+  function add_script(scriptURL,oID) {
+     var scriptEl = document.createElement("script");
+     var head=document.getElementsByTagName('head')[0];
+      scriptEl.type = "text/javascript";      
+      scriptEl.src = scriptURL;      
+      scriptEl.id=oID;      
+      head.appendChild(scriptEl);}
+
+//only want to add these once!
+if(document.getElementById('jquery')==null){
+add_script("https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js","jquery");
+
+}
+
+
+/* Get into full screen */
+function GoInFullscreen(preso) {
+	if(preso.requestFullscreen)
+		preso.requestFullscreen();
+	else if(preso.mozRequestFullScreen)
+		preso.mozRequestFullScreen();
+	else if(preso.webkitRequestFullscreen)
+		preso.webkitRequestFullscreen();
+	else if(preso.msRequestFullscreen)
+		preso.msRequestFullscreen();
+}
+
+/* Get out of full screen */
+function GoOutFullscreen() {
+	if(document.exitFullscreen)
+		document.exitFullscreen();
+	else if(document.mozCancelFullScreen)
+		document.mozCancelFullScreen();
+	else if(document.webkitExitFullscreen)
+		document.webkitExitFullscreen();
+	else if(document.msExitFullscreen)
+		document.msExitFullscreen();
+}
+
+/* Is currently in full screen or not */
+function IsFullScreenCurrently() {
+	var full_screen_preso = document.fullscreenpreso || document.webkitFullscreenpreso || document.mozFullScreenpreso || document.msFullscreenpreso || null;
+	
+	// If no preso is in full-screen
+	if(full_screen_preso === null)
+		return false;
+	else
+		return true;
+}
+
+GoInFullscreen($("#preso").get(0));
+
+}
+
