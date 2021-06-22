ced9d46862c39c9b4c5aeadf7ca88cee47a46497
aldip4886 <79340066+aldip4886@users.noreply.github.com>
2021-06-23 03:53:58 +0700

    Updated

--- KBM/MP1/story_content/user.js
+++ KBM/MP1/story_content/user.js
@@ -2,10 +2,10 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5iiBoqdJhSr":
+      case "5WDVv5SyaQ7":
         Script1();
         break;
-      case "5byY6wrRPj2":
+      case "5vmF39XIyTM":
         Script2();
         break;
   }
@@ -29,7 +29,7 @@ function add_line() {
 if(document.getElementById('bgSong')==null){
 	add_line();
 var audio = document.getElementById('bgSong');
-audio.volume = 1;
+audio.volume = 0.3;
 }
 
 }
@@ -40,7 +40,7 @@ function Script2()
 this.Location= player.GetVar("location");
 
 var audio = document.getElementById('bgSong');
-audio.src=Location+"musik.mp3";
+audio.src=Location+"Corporate Inspiring .mp3";
 audio.load();
 audio.play();
 }
--- KBM/MP3/story_content/user.js
+++ KBM/MP3/story_content/user.js
@@ -2,10 +2,10 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5W77yvazzfq":
+      case "5eRh9nwKvpq":
         Script1();
         break;
-      case "5XueJm0IaaJ":
+      case "5uPBEeaNTBf":
         Script2();
         break;
   }
@@ -29,7 +29,7 @@ function add_line() {
 if(document.getElementById('bgSong')==null){
 	add_line();
 var audio = document.getElementById('bgSong');
-audio.volume = 1;
+audio.volume = 0.07;
 }
 
 }
@@ -40,7 +40,7 @@ function Script2()
 this.Location= player.GetVar("location");
 
 var audio = document.getElementById('bgSong');
-audio.src=Location+"musik.mp3";
+audio.src=Location+"Corporation - no piano melody.mp3";
 audio.load();
 audio.play();
 }
