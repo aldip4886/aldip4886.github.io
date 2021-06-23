002c147a20c3b76d242de8a1dd0798b7c2d4c6c3
aldip4886 <79340066+aldip4886@users.noreply.github.com>
2021-06-23 18:01:32 +0700

    h

--- KBM/MP3/story_content/user.js
+++ KBM/MP3/story_content/user.js
@@ -2,10 +2,10 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "62HHCCEqYor":
+      case "5vTTB0xGkyk":
         Script1();
         break;
-      case "5bjzVrTU3lt":
+      case "5XyJpeBeya0":
         Script2();
         break;
   }
@@ -29,7 +29,7 @@ function add_line() {
 if(document.getElementById('bgSong')==null){
 	add_line();
 var audio = document.getElementById('bgSong');
-audio.volume = 0.3;
+audio.volume = 0.07;
 }
 
 }
@@ -40,7 +40,7 @@ function Script2()
 this.Location= player.GetVar("location");
 
 var audio = document.getElementById('bgSong');
-audio.src=Location+"Corporate Inspiring .mp3";
+audio.src=Location+"Corporation - no piano melody.mp3";
 audio.load();
 audio.play();
 }
