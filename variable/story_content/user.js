335e99b6afed5346ecf4322094aa1fa3e6daf7f8
aldip4886 <79340066+aldip4886@users.noreply.github.com>
2021-12-02 15:58:18 +0700

    ss

--- variable/story_content/user.js
+++ variable/story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6pUFvctA79V":
+      case "5Y7l4dh3aQW":
         Script1();
         break;
   }
@@ -12,28 +12,37 @@ function Script1()
 {
   var player = GetPlayer();
 
-WEB_APP_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=R9yAEDPmP_tR6Bmua7_Oy4zTf70GQnCLZVXrUI1N33qDTKEqFlr0o8Xw9h2b1h5RIiDkTozYSQ0-jMAJAkm-2qGpDG_QiXB4m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLrZtYeTndQ0Dgq1b3KN2t2QmRjEiDc2HucO9KBPuWISpgYDSOl3QjIHIAz1biNJfhHkjRWEXt1CgjFiQuiAa6k04qGmcNKFhg&lib=MDBures1_C4Aw3JYAiHuaw7egabXMlzsK";
+$.ajax({
 
-//copy and paste soryline variables
-//use a comma if you use multiple storyline variables
-storyline = 
+url:
+"https://script.googleusercontent.com/macros/echo?user_content_key=R9yAEDPmP_tR6Bmua7_Oy4zTf70GQnCLZVXrUI1N33qDTKEqFlr0o8Xw9h2b1h5RIiDkTozYSQ0-jMAJAkm-2qGpDG_QiXB4m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLrZtYeTndQ0Dgq1b3KN2t2QmRjEiDc2HucO9KBPuWISpgYDSOl3QjIHIAz1biNJfhHkjRWEXt1CgjFiQuiAa6k04qGmcNKFhg&lib=MDBures1_C4Aw3JYAiHuaw7egabXMlzsK",
+
+type: "POST",
+
+data: {"name": player.GetVar("name")
+, "user_JSExperience": player.GetVar("user_JSExperience")
+, "user_LMSRating": player.GetVar("user_LMSRating")
+, "user_SLExperience": player.GetVar("user_SLExperience")
+, "postname": player.GetVar("postname")
+, "postuser_JSExperience": player.GetVar("postuser_JSExperience")
+, "postuser_LMSRating": player.GetVar("postuser_LMSRating")
+, "postuser_SLExperience": player.GetVar("postuser_SLExperience")},
+
+
+success: function(data)
+
 {
-"user_JSExperience" : player.GetVar("user_JSExperience"),
-"user_LMSRating" : player.GetVar("user_LMSRating"),
-"user_SLExperience" : player.GetVar("user_SLExperience")
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
 }
-$.ajax(
-	{
-		url: WEB_APP_URL,
-		type: "POST",
-		data: storyline,
-		success: function(data);
-		},
-		error : function(err)
-		{
-			console.log('Error: ', err);
-		}
-	});
-	return false;
+
+});
+
+return false;
 }
 
