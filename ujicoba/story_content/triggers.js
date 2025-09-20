a9ae9f763aaeb39264c83c31257276ee95cc8f3c
aldip4886 <aldip4886@gmail.com>
2025-09-20 13:51:50 +0700

    1.2

---
+++ ujicoba/story_content/triggers.js
@@ -0,0 +1,38 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ZChgAWfzmy":
+        Script1();
+        break;
+      case "5iJ4Tklwe2V":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
+function getActor() {
+  return {
+    "objectType": "Agent",
+    "account": {
+      "homePage": "https://www.example.com",
+      "name": uname
+    }
+  };
+}
