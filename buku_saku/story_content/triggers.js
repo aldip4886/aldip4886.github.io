b0b6566ae688dd4e76afe6c93b7dca7534aa02b1
aldip4886 <aldip4886@gmail.com>
2025-09-28 19:18:26 +0700

    s

--- buku_saku/story_content/triggers.js
+++ buku_saku/story_content/triggers.js
@@ -21,3 +21,12 @@ var hidePointer = player.hidePointer;
 var slideWidth = player.slideWidth;
 var slideHeight = player.slideHeight;
 };
+function getActor() {
+  return {
+    "objectType": "Agent",
+    "account": {
+      "homePage": "https://www.example.com",
+      "name": "Random" + Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+    }
+  };
+}
